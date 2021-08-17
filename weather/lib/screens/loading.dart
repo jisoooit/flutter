82278c50a4701888597e38af4a0eca24cf59d1a4
jisoooit <ydsn336@naver.com>
2021-08-17 14:49:41 +0900

    weather lecture 1617

--- weather/lib/screens/loading.dart
+++ weather/lib/screens/loading.dart
@@ -3,46 +3,54 @@ import 'package:geolocator/geolocator.dart';
 import 'package:weather/data/network.dart';
 import 'package:weather/data/mylocation.dart';
 import 'package:weather/screens/weatherscreen.dart';
-const apiKey='6fec830b749ff5fcbf0980a4d0f5a5c0';
+
+const apiKey = '6fec830b749ff5fcbf0980a4d0f5a5c0';
 
 class Loading extends StatefulWidget {
-  const Loading({ Key? key }) : super(key: key);
+  const Loading({Key? key}) : super(key: key);
 
   @override
   _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+  double latitude3 = 0;
+  double longitude3 = 0;
 
-double latitude3=0;
-double longitude3=0;
-
-@override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getLocation();
-    
   }
 
-  void getLocation() async{
-    Mylocation myLocation= Mylocation();
+  void getLocation() async {
+    Mylocation myLocation = Mylocation();
     await myLocation.getMyCurrentLocation();
-    latitude3=myLocation.latitude2;
-    longitude3=myLocation.longitude2;
+    latitude3 = myLocation.latitude2;
+    longitude3 = myLocation.longitude2;
     print(latitude3);
     print(longitude3);
 
-    Network network=Network('https://api.openweathermap.org/data/2.5/weather?lat=$latitude3&lon=$longitude3&appid=$apiKey&units=metric');
-    var weatherData=await network.getJsonData();
+    Network network = Network(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude3&lon=$longitude3&appid=$apiKey&units=metric',
+        'https://api.openweathermap.org/data/2.5/air_pollution?lat=$latitude3&lon=$longitude3&appid=$apiKey');
+    var weatherData = await network.getJsonData();
     print(weatherData);
-    Navigator.push(context, MaterialPageRoute(builder: (context){
-      return WeatherScreen(parseWeatherData: weatherData,);
+
+    var airData = await network.getAirData();
+    print(airData);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return WeatherScreen(
+        parseWeatherData: weatherData,
+        parseAirData: airData,
+      );
     }));
   }
 
   // void fetchData() async{
-    
+
   //     var myjson=parsingData['weather'][0]['description'];
   //     print(myjson);
 
@@ -63,15 +71,14 @@ double longitude3=0;
       body: Center(
         child: RaisedButton(
           onPressed: null,
-          child: Text('get my location',
-          style: TextStyle(
-            color: Colors.white,
+          child: Text(
+            'get my location',
+            style: TextStyle(
+              color: Colors.white,
             ),
           ),
           color: Colors.blue,
-          
         ),
-
       ),
     );
   }
--- weather/lib/screens/weatherscreen.dart
+++ weather/lib/screens/weatherscreen.dart
@@ -1,58 +1,280 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
+import 'package:weather/model/model.dart';
 
 class WeatherScreen extends StatefulWidget {
-  WeatherScreen({this.parseWeatherData});
+  WeatherScreen({this.parseWeatherData,this.parseAirData});
   final dynamic parseWeatherData;
- 
-
+  final dynamic parseAirData;
   @override
   _WeatherScreenState createState() => _WeatherScreenState();
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  
-  String city="";
-  int temp=0;
-  
+  String city = "";
+  int temp = 0;
+  Widget? icon;
+  String des="";
+  Widget? airIcon;
+  Widget? airState;
+  double dust1=0;
+  double dust2=0;
+  Model model=Model();
+  var date=DateTime.now();
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    updateData(widget.parseWeatherData);
+    updateData(widget.parseWeatherData,widget.parseAirData);
   }
 
-  void updateData(dynamic weatherData){
+  void updateData(dynamic weatherData,dynamic airData) {
+    int condition=weatherData['weather'][0]['id'];
+    des=weatherData['weather'][0]['description'];
+    int index=airData['list'][0]['main']['aqi']; 
+    dust1=airData['list'][0]['components']['pm10'];
+    dust2=airData['list'][0]['components']['pm2_5'];
 
-    double temp2=weatherData['main']['temp'];
-    temp=temp2.toInt(); //round() 반올림 해도 됨 ㅋ
-    city=weatherData['name'];
+    double temp2 = weatherData['main']['temp'];
+    temp = temp2.toInt(); //round() 반올림 해도 됨 ㅋ
+    city = weatherData['name'];
+    icon=model.getWeatherIcon(condition);
+    airIcon=model.getAirIcon(index);
+    airState=model.getAirCondition(index);
     print(temp);
     print(city);
+  }
+
+  String getSystemTime(){
+    var now=DateTime.now();
+    return DateFormat("h:mm a").format(now);
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      extendBodyBehindAppBar: true, //바디를 앱바까지 확장
+      appBar: AppBar(
+        title: Text(''),
+        backgroundColor: Colors.transparent, //색상이 사라짐, 회색
+        elevation: 0.0, //음영 없애기
+        leading: IconButton(
+          icon: Icon(Icons.near_me),
+          onPressed: () {},
+          iconSize: 30.0,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_searching),
+            onPressed: () {},
+            iconSize: 30.0,
+          ),
+        ],
+      ),
+      body: Container(
+        child: Stack(
           children: [
-            Text('$city',
-            style:TextStyle(
-              fontSize:30.0
+            Image.asset(
+              'image/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ), //지정한 영역을 꽉채우기 위해서
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded( //확보할 수 있는 최대공간 확보
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 150.0,
+                            ),
+                            Text(
+                              '$city',
+                              style: GoogleFonts.lato(
+                                fontSize: 35.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                TimerBuilder.periodic(
+                                  (Duration(minutes: 1)),
+                                  builder: (context){ //빌더 위젯은 반드시 위젯을 리턴해줘야 한다.
+                                    print('${getSystemTime()}');
+                                    return Text(
+                                      '${getSystemTime()}',
+                                      style: GoogleFonts.lato(
+                                        fontSize:16.0,
+                                        color:Colors.white,
+                                      ),
+                                    );
+                                  },
+                                ),
+                                Text(
+                                  DateFormat('- EEEE, ').format(date),
+                                  style: GoogleFonts.lato(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                 Text(
+                                  DateFormat('d MMM, yyy').format(date),
+                                  style: GoogleFonts.lato(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                             Text(
+                              '$temp\u2103',
+                              style: GoogleFonts.lato(
+                                fontSize: 85.0,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Row(
+                              children:[
+                                icon!,
+                                SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text(
+                                  '$des',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ]
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Divider(
+                        height: 15.0,
+                        thickness: 2.0,
+                        color: Colors.white30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                         Column(
+                           children: [
+                              Text(
+                                'aqi',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              airIcon!,
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              airState!,
+                           ],
+                         ),
+                         Column(
+                           children: [
+                              Text(
+                                '미세먼지',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '$dust1',
+                                style: GoogleFonts.lato(
+                                  fontSize: 24.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                              'm3',
+                              style: GoogleFonts.lato(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                                fontWeight:FontWeight.bold
+                              ),
+                            ),
+                           ],
+                         ),
+                         Column(
+                           children: [
+                              Text(
+                                '초미세먼지',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '$dust2',
+                                style: GoogleFonts.lato(
+                                  fontSize: 24.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                              'm3',
+                              style: GoogleFonts.lato(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                                fontWeight:FontWeight.bold
+                              ),
+                            ),
+                           ],
+                         )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text('$temp',
-            style:TextStyle(
-              fontSize:30.0
-            ),
-            ),
           ],
         ),
       ),
-        
     );
   }
 }
